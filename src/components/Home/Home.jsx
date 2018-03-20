672470f216d40ee00b62b519e659eeddc422f648
Rob Sawyer <rsawyer@tripwire.com>
2018-03-20 09:12:20 +0900

    Updated the images folder for Home and tweaked the copy a bit.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,13 +4,12 @@ import { inject, observer } from 'mobx-react';
 import { Match, Link, withRouter } from 'react-router-dom';
 
 // Components
-import Stripes from '../Stripes';
-import Header from '../Header';
-import Pricing from '../Pricing';
-import MaterialRaisedButton from '../forms/MaterialRaisedButton';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import { DefaultPlayer as Player } from 'react-html5video';
+
+// Component Styles
+import 'react-html5video/dist/styles.css';
 
 // Icons
 import MdDvr from 'react-icons/lib/md/dvr';
@@ -24,31 +23,35 @@ import MdFormatAlignCenter from 'react-icons/lib/md/format-align-center';
 import TiArrowRightThick from 'react-icons/lib/ti/arrow-right-thick';
 import PhoneLinkLock from 'react-icons/lib/md/phonelink-lock';
 
+// Local Components
+import Footer from '../Footer';
+import Stripes from '../Stripes';
+import Header from '../Header';
+import Pricing from '../Pricing';
+import MaterialRaisedButton from '../forms/MaterialRaisedButton';
+
 // Styles
-import 'react-html5video/dist/styles.css';
 import styles from './Home.css';
 
-import banner from '../../images/banner.jpg';
-
 // static html files
+// import projectHtml from '../../static/project.html';
+// import singleHtml from '../../static/single.html';
+// import followupHtml from '../../static/followup.html';
+// import smallProjectHtml from '../../static/small-project.html';
+// import mediumProjectHtml from '../../static/medium-project.html';
+// import largeProjectHtml from '../../static/large-project.html';
+// import commentHtml from '../../static/comments.html';
 
-import projectHtml from '../../static/project.html';
-import singleHtml from '../../static/single.html';
-import followupHtml from '../../static/followup.html';
-import smallProjectHtml from '../../static/small-project.html';
-import mediumProjectHtml from '../../static/medium-project.html';
-import largeProjectHtml from '../../static/large-project.html';
-import commentHtml from '../../static/comments.html';
-
-// video
+// Videos
 import demoVideo from '../../video/get-code-cleaner-demo.mp4';
 
-// images
-import airbnb from './img/logos/airbnb-300.png';
-import github from './img/logos/github-200.png';
-import google from './img/logos/google-300.png';
-import paypal from './img/logos/paypal-300.png';
-import microsoft from './img/logos/microsoft-300.png';
+// Images
+import banner from '../images/banner.jpg';
+import airbnb from './images/logos/airbnb-300.png';
+import github from './images/logos/github-200.png';
+import google from './images/logos/google-300.png';
+import paypal from './images/logos/paypal-300.png';
+import microsoft from './images/logos/microsoft-300.png';
 
 @inject("store")
 @withRouter
@@ -64,8 +67,8 @@ export default class Home extends Component {
 
     const { id } = this.props.match.params;
 
-    const sectionHeader = "f3 lh-title fw2 tl mint-green";
-    const sectionCopy = "f5 fw2 lh-copy mint-gray";
+    const sectionHeader = 'f3 lh-title fw2 tl mint-green ttu';
+    const sectionCopy = 'f5 fw2 lh-copy mint-gray';
 
     return (
       <div className="absolute w-100 home z-1">
@@ -74,25 +77,25 @@ export default class Home extends Component {
           <Header id={id} />
         </header>
         <div className="cf relative w-100 z-2 mt6-ns mt6-m mt7">
-          <div className="fl w-30-ns w-0 w-0-m pa2-ns pa0 pa0-m"></div>
+          <div className="fl w-30-ns w-0 w-0-m pa2-ns pa0 pa0-m" />
           <div className="fl w-60-ns w-100 w-100-m pa2 tr">
             <h2 className={`${sectionHeader}`}>
-              WE WORK WITH ANY SIZE PROJECT
+              Lorem Ipsum is simply dummy text of the
             </h2>
-            {/* <small>Small projects are less than ten (10) files.</small> */}
+            {/* <small>Not only five centuries, but also the leap into electronic.</small> */}
             <ul className={`${sectionCopy} cf tl pv3 list pl0`}>
               <li className="cf w-100 pv2">
                 <PhoneLinkLock className="fl f2 w-20 pt2" />
-                <div className="fl f5 lh-copy w-75 pt0">Worried about intellectual property? Signing an NDA is no problem.</div>
+                <div className="fl f5 lh-copy w-75 pt0">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
               </li>
               <li className="cf w-100 pv2">
                 <MdLockOutline className="fl f2 w-20 pt2" />
-                <div className="fl f5 lh-copy w-75 pt0">Code in a private repo? We're very familiar with online repositories such as Github. Just add <strong><a href="https://www.github.com/getcleanercode" className="link purple dim fb" target="_blank">getcleanercode</a></strong> to the repo and we're on it.</div>
+                <div className="fl f5 lh-copy w-75 pt0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac nisi in mauris tincidunt bibendum. </div>
               </li>
             </ul>
             <a href="/project" className="big-link db pv4 dim mt3 w-100">Start the process <TiArrowRightThick /></a>
           </div>
-          <div className="fl w-20 w-20-ns w-0 w-0-m pa2-ns pa0 pa0-m"></div>
+          <div className="fl w-20 w-20-ns w-0 w-0-m pa2-ns pa0 pa0-m" />
         </div>
 
         {/* Pricing */}
@@ -115,12 +118,12 @@ export default class Home extends Component {
           </div>
           <div className="fl w-80 pa2">
             <h2 className={sectionHeader}>
-              <span className="">What are the advantages of using web standards?</span>
+              <span className="">Duis id ipsum dui?</span>
             </h2>
             <ul className={`${sectionCopy} pv3`}>
-              <li>Greater visibility in web searches</li>
-              <li>Useful for people with disabilities</li>
-              <li>More forward- and backward-compatibile</li>
+              <li>Vestibulum porttitor ligula in velit blandit fringilla</li>
+              <li>Sed cursus turpis at ullamcorper convallis</li>
+              <li>A rhoncus neque scelerisque</li>
             </ul>
             <div className="cf w-100 f7">
               <div className="fl-ns w-third-ns w-100 pv0-ns pv2 pa2">
@@ -141,23 +144,23 @@ export default class Home extends Component {
 
         <div className="w-75-ns w-100-m w-100 center tc pv5-ns pv3">
           <div className="dib-ns w-20-ns w-40-m w-50 center ph3">
-            <img src={airbnb} className="h2 pv3" />
+            <img src={airbnb} className="h2 pv3" alt="airbnb" />
           </div>
           <div className="dib-ns w-20-ns w-40-m w-50 center ph3">
-            <img src={microsoft} className="h2 pv3" />
+            <img src={microsoft} className="h2 pv3" alt="microsoft" />
           </div>
           <div className="dib-ns w-20-ns w-40-m w-50 center ph3">
-            <img src={paypal} className="h2 pv3" />
+            <img src={paypal} className="h2 pv3" alt="paypal" />
           </div>
           <div className="dib-ns w-20-ns w-40-m w-50 center ph3">
-            <img src={github} className="h2 pv3" />
+            <img src={github} className="h2 pv3" alt="github" />
           </div>
           <div className="dib-ns w-20-ns w-40-m w-50 center ph3">
-            <img src={google} className="h2 pv3" />
+            <img src={google} className="h2 pv3" alt="google" />
           </div>
         </div>
 
-        <div className="w-100 pv2 grad3"></div>
+        <div className="w-100 pv2 grad3" />
 
         <div className="cf w-75-ns w-100-m w-100 center tl pv5">
           <div className="fl w-20 tr pa2">
@@ -165,20 +168,17 @@ export default class Home extends Component {
           </div>
           <div className="fl w-80 pa2">
             <h2 className={sectionHeader}>
-              <span className="">We're serious about clean code.</span>
+              <span className="">We&apos;re serious about nothing.</span>
             </h2>
             <p className={`${sectionCopy} pv3`}>
-              Our team of engineers and code cleaners use a collection of tools and hand editing to ensure your code meets the spec provided.
+              Fusce volutpat vulputate auctor. Duis id ipsum dui. Quisque
+              facilisis purus ut nisl dignissim, a rhoncus neque scelerisque.
             </p>
-            {/* <a href="#" className="big-link db pv4 dim mt3 w-100">Start the process <TiArrowRightThick /></a> */}
           </div>
         </div>
-
-        <div className="cf w-100 f7 center white bt bw2 b--mint-light-gray pv5 tc grad2">
-          <span>Copyright &copy; 2017 Get Cleaner Code</span>
-        </div>
-
-
+        <Footer>
+          Copyright &copy; 2018 React Startup Template
+        </Footer>
       </div>
     );
   }
